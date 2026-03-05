dedc751170513bbf115ab042f0881cf045cd6b46
persrach <rachid.iouaj.official@gmail.com>
2026-03-05 07:50:18 +0000

    Nothing

--- time_server.c
+++ time_server.c
@@ -55,35 +55,35 @@ int main(){
 
 	}
 
-	printf("CLient is connected...");
+	printf("CLient is connected...\n");
 	char address_buffer[100];
 	getnameinfo((struct sockaddr*)&client_address, client_len, address_buffer, sizeof(address_buffer), 0, 0, NI_NUMERICHOST);
-	printf("%s/n", &address_buffer);
+	printf("%s\n", &address_buffer);
 
-	printf("Reading request...");
+	printf("Reading request...\n");
 	char request[1024];
 	int byte_received = recv(socket_client, request, 1024, 0);
 	printf("Received %d bytes.\n", byte_received);
 
-	printf("Sending response...");
+	printf("Sending response...\n");
 	const char *response = 
 		"HTTP/1.1 200 OK\r\n"
 		"Connection: close\r\n"
 		"Content-Type: text/plain\r\n\r\n"
 		"Local time is: ";
 	int byte_sent = send(socket_client, response, strlen(response), 0);
-	printf("Sent %d of %d bytes", byte_sent, (int)strlen(response));
+	printf("Sent %d of %d bytes\n", byte_sent, (int)strlen(response));
 	
 	time_t timer;
 	time(&timer);
 	char *time_msg = ctime(&timer);
 	byte_sent = send(socket_client, time_msg, strlen(time_msg), 0);
-	printf("Sent %d of %d bytes", byte_sent, (int)strlen(time_msg));
+	printf("Sent %d of %d bytes\n", byte_sent, (int)strlen(time_msg));
 	
-	printf("Closing connection..");
+	printf("Closing connection..\n");
 	close(socket_client);
 
-	printf("Finished./n");
+	printf("Finished.\n");
 	
 	return 0;
 }
